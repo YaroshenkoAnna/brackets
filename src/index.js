e293843cad6b9e23e49892eff2e2aae6cfbfb3ed
Anna <yaroshenkoannette@gmail.com>
2024-08-06 09:45:51 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,30 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  let bracketsObj = {};
-
-  bracketsConfig.forEach((elem) => {
-    bracketsObj[elem[0]] = elem[1];
-  })
-  let bracketsArr = str.split("");
-
-  function checkIt(){ debugger
-    for (let i = 0; i < bracketsArr.length; i++) {
-        if (!bracketsObj.hasOwnProperty([bracketsArr[i]])) {
-          
-           if (bracketsObj[bracketsArr[i-1]]===bracketsArr[i]) {
-             bracketsArr.splice(i-1, 2);
-        
-             checkIt();
-           } else {
-             return false
-             break;
-           } 
-        }
-    }
-    return true;
-  }
-
- return checkIt();
-
+  return (
+    str.split('').reduce((acc, cur) => {
+      Object.fromEntries(bracketsConfig)[acc.at(-1)] === cur
+        ? acc.pop()
+        : acc.push(cur);
+      return acc;
+    }, []).length === 0
+  );
 }
+
 
 
